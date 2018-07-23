84569d510ac315abfdf51960b9b2e1fd74f450ce
vince4128 <vrlt.vincent@gmail.com>
2018-07-23 16:32:28 +0200

    user creation

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,32 @@
+const User = require('../models/user');
+
 exports.signup = function(req,res,next){
-    res.send({ success: 'true'});
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //see if user with a given email exist
+    User.findOne({ email: email}, function(err, existingUser) {
+        if(err) { return next(err);}
+
+        //if user with email does exist, return an error
+        if(existingUser) {
+            return res.status(422).send({error: 'Email is in use'});
+        }
+
+        //if user with email does not exist, create and save user record
+        const user = new User({
+            email:email,
+            password:password
+        });
+
+        user.save(function(err){
+            if(err) {return next(err); }
+
+            // Respond to request indicating the user was created
+            res.json({ success: true});
+        });
+
+        //Repond to request indicating the user was created
+
+    });    
 };
