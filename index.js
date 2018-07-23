88fbd33e96c9474c2c0e8931d9597d48fce7f517
vince4128 <vrlt.vincent@gmail.com>
2018-07-23 14:57:22 +0200

    basic route added

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+const router = require('./router');
 
 // App Setup
 app.use(morgan('combined'));// log
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
