db9d66258bdf6f56ef3def27888e96d6dfea3c8c
vince4128 <vrlt.vincent@gmail.com>
2018-07-23 15:30:35 +0200

    connect to db

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = require('./router');
+
+// DB Setup
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1/auth?connectTimeoutMS=5000').then(
+    () => {console.log("connecté à la base de donnée !")},
+    err => console.log("erreur de connexion à la base de donnée ", err.message)
+);
 
 // App Setup
 app.use(morgan('combined'));// log
